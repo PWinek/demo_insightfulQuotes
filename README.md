22fe7ef2876971cbd24b74af48f7c887f0723e6f
PWinek <pwinek@gmail.com>
2020-06-16 12:37:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-fy8ols
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-fy8ols)
---
+++ src/app/models/database.ts
@@ -0,0 +1,34 @@
+import { Quotation } from './quotation';
+
+export const QUOTES: Quotation[] = [
+  {
+    author: 'Albert Einstein',
+    sentence: 'Jak to się dzieje, że nikt mnie nie rozumie, a wszyscy mnie uwielbiają?',
+    votes: 5,
+  },
+  {
+    author: 'Bill Gates',
+    sentence: 'Wiedza to najlepsze narzędzie władzy.',
+    votes: 2,
+  },
+  {
+    author: 'George Eliot',
+    sentence: 'Nigdy nie jest za późno, by stać się tym, kim chcemy być.',
+    votes: 1,
+  },
+  {
+    author: 'Stanisław Lem',
+    sentence: 'Bądź dobrej myśli, bo po co być złej.',
+    votes: 3,
+  },
+  {
+    author: 'Tove Jansson',
+    sentence: 'Mówienie tak bardzo przeszkadza w myśleniu.',
+    votes: -8,
+  },
+  {
+    author: 'Peter Lippert',
+    sentence: 'Życia nie można wybrać, ale można z niego coś zrobić.',
+    votes: 0,
+  },
+];
